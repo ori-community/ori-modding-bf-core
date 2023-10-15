37fdc1f1584ed9bcac5f86e4edd359905e1e8dde
Kirefel <kirefelan@gmail.com>
2023-10-15 23:07:41 +1100

    File path helpers

--- OriModding.BF.Core/HarmonyHelper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OriModding.BF.Core;
-
-public static class HarmonyHelper
-{
-    public const bool ContinueExecution = true;
-    public const bool StopExecution = false;
-}
---
+++ OriModding.BF.Core/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace OriModding.BF.Core;
+
+public static class HarmonyHelper
+{
+    /// <summary>Return this (or void) from a Prefix to allow the original method to run</summary>
+    public const bool ContinueExecution = true;
+    /// <summary>Return from a Prefix to stop the original method from running, but does not stop other prefixes</summary>
+    public const bool StopExecution = false;
+}
+
+public static class FilesystemHelpers
+{
+    public static string GetPluginDirectory(this Plugin plugin)
+    {
+        return Path.GetDirectoryName(plugin.Info.Location);
+    }
+
+    public static string GetAssetPath(this Plugin plugin, params string[] relativePath)
+    {
+        return Path.Combine(plugin.GetPluginDirectory(), CombinePath(relativePath));
+    }
+
+    public static string CombinePath(params string[] paths)
+    {
+        string path = "";
+        for (int i = 0; i < paths.Length; i++)
+            path = Path.Combine(path, paths[i]);
+        return path;
+    }
+}
